11adec68ec7d373dcb1765fd3f58f0751457ca92
EmanuelaPau <uapame@gmail.com>
2023-06-19 14:50:30 +0200

    minor edit to app vue setup

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <HelloWorld>
+    </HelloWorld>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style lang ="scss">
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
 @import './styles/general/scss'
 </style>
