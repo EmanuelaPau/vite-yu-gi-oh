8bcc9445322028898c81909d66e3e2cfd007e0fa
EmanuelaPau <uapame@gmail.com>
2023-06-20 18:53:14 +0200

    Problem on making emit work

--- src/components/CardContainer.vue
+++ src/components/CardContainer.vue
@@ -7,7 +7,7 @@
                     <h2>Found {{ store.cardList.length }} cards</h2>
                 </div>
                 <AppLoader v-if="store.cardList.length === 0" />
-                <div @select="clickTest">Ciao {{ scritta }}</div>
+                <div @search="console.log('daiii')">Ciao {{ scritta }}</div>
                 <CardElement class="col-lg-3 col-md-6 col-12" v-for="card in store.cardList" :cardName="card.name"
                     :cardType="card.type" :cardImage="card.card_images[0].image_url">
                 </CardElement>
