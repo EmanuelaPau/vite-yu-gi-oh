be204956d560ae7f1deda56fd60cc9201e64b292
EmanuelaPau <uapame@gmail.com>
2023-06-20 12:37:07 +0200

    Try to get only 20 archetypes from api

--- src/components/AppSearchbar.vue
+++ src/components/AppSearchbar.vue
@@ -30,7 +30,7 @@ export default {
 
     created() {
         // Make a request for a user with a given ID
-        axios.get('https://db.ygoprodeck.com/api/v7/archetypes.php')
+        axios.get('https://db.ygoprodeck.com/api/v7/archetypes.php?archetype_name=20')
             .then((response) => {
                 // handle success
                 console.log(response)
