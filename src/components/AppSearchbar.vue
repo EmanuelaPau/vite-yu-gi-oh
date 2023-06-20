48d3b7ae2f14baafa83e930ed08f1b7baaf3787c
EmanuelaPau <uapame@gmail.com>
2023-06-20 12:07:30 +0200

    Testing creating dropdown menu li

--- src/components/AppSearchbar.vue
+++ src/components/AppSearchbar.vue
@@ -1,13 +1,11 @@
 <template>
     <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown"
-            aria-expanded="false">
-            Alien
+        <button class="btn btn-secondary dropdown-toggle my_button" type="button" id="dropdownMenuButton1"
+            data-bs-toggle="dropdown" aria-expanded="false">
+            Choose the archetype
         </button>
         <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li v-for="archetype in store.cardList"><a class="dropdown-item" href="#">Action</a></li>
         </ul>
     </div>
 </template>
@@ -34,6 +32,12 @@ export default {
 <style scoped lang="scss">
 @use '../styles/partials/_variables.scss';
 @use '../styles/partials/_mixins.scss';
+
+button.my_button {
+    background-color: white;
+    color: black;
+    border: none;
+}
 
 div.dropdown {
     margin-bottom: 30px;
